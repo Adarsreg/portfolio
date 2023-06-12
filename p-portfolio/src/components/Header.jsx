da482d23691240713bdf067ec1063107ea575974
Adarsh <adarshregmi1@gmail.com>
2023-06-12 22:57:23 +0530

    Updated background

--- p-portfolio/src/components/Header.jsx
+++ p-portfolio/src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <div className="Header bg-gradient-to-r from-gray-400 to-gray-900">
+        <div className="bg-gray-800">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <h1 className="text-center text-4xl font-bold text-white tracking-tight">
-                    Hi, I'm Adarsh
+                    Hi, I'm Adarsh Sharma
                 </h1>
                 <p className="mt-4 max-w-2xl mx-auto text-center text-xl text-gray-300">
                     Welcome to my portfolio
