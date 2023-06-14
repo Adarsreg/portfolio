10ac5b4e09f529fcbfbbb0ab5032891b307d4a2f
Adarsh <adarshregmi1@gmail.com>
2023-06-14 17:13:00 +0530

    Updated header| added pfp

--- p-portfolio/src/components/Header.jsx
+++ p-portfolio/src/components/Header.jsx
@@ -1,13 +1,20 @@
+import pfp from '../images/pfpp.jpg'
+
 const Header = () => {
     return (
         <div className="bg-gray-800">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-center text-4xl font-bold text-white tracking-tight" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+                <h1 className="text-center text-4xl md:text-5xl font-bold text-white tracking-tight" style={{ fontFamily: 'Montserrat, sans-serif' }}>
                     Hi, I'm Adarsh Sharma
                 </h1>
-                <p className="mt-4 max-w-2xl mx-auto text-center text-xl text-gray-300">
+                <p className="mt-4 max-w-2xl mx-auto text-center text-xl md:text-2xl lg:text-3xl text-gray-300 ">
                     Welcome to my portfolio
                 </p>
+                <div className="text-center my-5">
+                    <div className="inline-block rounded-full border-4 border-gray-300 overflow-hidden ">
+                        <img src={pfp} alt="Avatar" className="object-center max-h-60 md:max-h-80" />
+                    </div>
+                </div>
             </div>
         </div>
     );
