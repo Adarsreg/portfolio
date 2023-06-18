c7c4a3d37863e2e8ce78e76656586d64c244bed4
Adarsh <adarshregmi1@gmail.com>
2023-06-18 16:23:55 +0530

    mapped projects

--- p-portfolio/src/components/Works.jsx
+++ p-portfolio/src/components/Works.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import ProjectCard from '../layouts/Projectcard';
-import notegpt from '../images/notegpt.png';
+import projects from '../data/projects';
 
 const Works = () => {
     return (
         <div className="bg-slate-900 py-10 rounded-3xl mx-auto max-w-6xl shadow-lg">
             <div className="max-w-2xl mx-auto">
-                <p className="-mt-15 lg:text-2xl my-20 text-purple-500 mt-10 font-semibold text-center text-2xl">Here are some of my projects...</p>
-
-                <ProjectCard
-                    title="NOTEGPT"
-                    description="Notes app integrated with ChatGPT's API"
-                    imageUrl={notegpt}
-                    liveUrl=""
-                    sourceUrl="https://github.com/Adarsreg/Notegpt"
-                />
+                <p className="-mt-15 lg:text-2xl my-20 text-purple-500 mt-10 font-semibold text-center text-2xl">
+                    Here are some of my projects...
+                </p>
+                <div className="space-y-6">
+                    {projects.map((project) => (
+                        <div className="mb-6" key={project.title}>
+                            <ProjectCard
+                                title={project.title}
+                                description={project.description}
+                                imageUrl={project.imageUrl}
+                                liveUrl={project.liveUrl}
+                                sourceUrl={project.sourceUrl}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
---
+++ p-portfolio/src/data/projects.js
@@ -0,0 +1,34 @@
+import notegpt from '../images/notegpt.png'
+
+const projects = [
+    {
+        title: "NOTEGPT",
+        description: "Notes app integrated with ChatGPT's API",
+        imageUrl: notegpt,
+        liveUrl: "", // Add the live URL for the project here
+        sourceUrl: "https://github.com/Adarsreg/Notegpt"
+    },
+    {
+        title: "Project 2",
+        description: "Description for Project 2",
+        imageUrl: "https://via.placeholder.com/640x360.png?text=Placeholder+Image",
+        liveUrl: "", // Add the live URL for the project here
+        sourceUrl: ""
+    },
+    {
+        title: "Project 3",
+        description: "Description for Project 3",
+        imageUrl: "https://via.placeholder.com/640x360.png?text=Placeholder+Image",
+        liveUrl: "", // Add the live URL for the project here
+        sourceUrl: ""
+    },
+    {
+        title: "Project 4",
+        description: "Description for Project 4",
+        imageUrl: "https://via.placeholder.com/640x360.png?text=Placeholder+Image",
+        liveUrl: "", // Add the live URL for the project here
+        sourceUrl: ""
+    }
+];
+
+export default projects;
