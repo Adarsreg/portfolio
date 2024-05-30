433d4d97bf6148126a76c90d94c8476d8bf43f4c
Adarsh <adarshregmi1@gmail.com>
2024-05-30 22:06:44 +0530

    updated footer

--- p-portfolio/src/components/Footer.jsx
+++ p-portfolio/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="container mx-auto px-4">
                 <div className="flex flex-wrap items-center justify-between">
                     <div className="w-full sm:w-auto order-2 sm:order-1 text-center sm:text-left mb-4 sm:mb-0">
-                        <p className="text-gray-400">© 2023 Adarsh Sharma. All rights reserved.</p>
+                        <p className="text-gray-400">© 2024 Adarsh Sharma. All rights reserved.</p>
                     </div>
                     <div className="w-full sm:w-auto order-1 sm:order-2 text-center sm:text-right">
 
